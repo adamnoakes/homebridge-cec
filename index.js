af4343fd11ff7011d4d8c4ed6d4eab6fa9eec78e
Adam Noakes <acn2g13@soton.ac.uk>
2016-10-16 17:23:44 +0100

    Fixes, now working.

--- index.js
+++ index.js
@@ -1,48 +1,55 @@
-const spawn = require('spawn');
-let Service, Characteristic, UUIDGen;
+const exec = require('child_process').exec;
+let Service, Characteristic;
 
 module.exports = (homebridge) => {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerPlatform("homebridge-cec", "TV", CEC);
+  homebridge.registerAccessory("homebridge-cec", "TV", CEC);
 }
 
 class CEC{
   constructor(log, config, api){
+
     this.log = log;
     this.config = config;
-
-    this.service = new Service.Switch(this.name);
+    this.log('Creating TV CEC Accessory');
+    this.service = new Service.Switch('TV');
 
     this.service
       .getCharacteristic(Characteristic.On)
-      .on('set', this.setPower.bind(this));
+      .on('set', this.setPower.bind(this))
       .on('get', this.getPower.bind(this));
   }
 
   setPower(on, cb) {
+    this.log(`Set power ${on}`);
     const cmd = on ? 'on' : 'standby';
     exec(`echo "${cmd} 0" | cec-client -s`, (err, stdout, stderr) => {
       if (err !== null) {
-        console.log('exec error: ' + error);
+        this.log('exec error: ' + err);
       }
       cb(null, on);
     });
-  },
+  }
 
-  getPower(on, cb) {
+  getPower(cb) {
     exec(`echo "pow 0" | cec-client -s`, (err, stdout, stderr) => {
       if (err !== null) {
-        console.log('exec error: ' + error);
+        this.log('exec error: ' + err);
       }
-      let n = stdout.indexOf("power status:");
-      status_temp = stdout.substring(n+13, n+28);
-      status = status_temp.substring(0, status_temp.indexOf("D")-1);      
-      res.render(null, status === 'on');
+      const n = stdout.indexOf("power status:");
+      const status = stdout.substring(n+14, n+16);
+      cb(null, status === 'on');
     });
-  },
+}
+
+  identify(callback) {
+    this.log("Identify requested!");
+    callback(); // success
+  }
 
   getServices() {
+    this.log("Get services");
     return [this.service];
   }
 }
